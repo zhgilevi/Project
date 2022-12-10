de06690b8af1dadca2b9b3d5f30578bff5180889
Zhgilev Ilya <st086873@student.spbu.ru>
2022-12-10 19:18:16 +0300

    initial commit

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,10 +1,20 @@
 package com.example.demo.config;
 
+import jakarta.servlet.http.HttpFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+    return http.csrf().disable().authorizeRequests().anyRequest().permitAll()
+        .and().build();
+  }
 
 
 
