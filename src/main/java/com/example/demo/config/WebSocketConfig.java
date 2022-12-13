a582b9a4b1a8c268317e5b6acba32d4bbb09fc18
Zhgilev Ilya <st086873@student.spbu.ru>
2022-12-14 00:17:51 +0300

    web socket fixes

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -23,8 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
   @Override
   public void   configureMessageBroker(MessageBrokerRegistry config){
-    config.enableSimpleBroker("/chat");
-    config.setApplicationDestinationPrefixes("/app");
+    config.enableSimpleBroker("/ws");
   }
 
   @Override
