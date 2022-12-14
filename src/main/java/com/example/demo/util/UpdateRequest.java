db520b1ffd6fdb5e87cd742c3904c29aed3077d4
Zhgilev Ilya <st086873@student.spbu.ru>
2022-12-14 19:42:54 +0300

    update added

--- src/main/java/com/example/demo/util/UpdateRequest.java
+++ src/main/java/com/example/demo/util/UpdateRequest.java
@@ -1,23 +1,25 @@
 package com.example.demo.util;
 
+
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.java.Log;
 
 @Getter
 @Setter
-public class SignUpRequest {
+public class UpdateRequest {
 
+  private Long id;
   private String username;
   private String fName;
   private String lName;
   private String password;
 
-  public SignUpRequest(String username, String fName, String lName, String password) {
+  public UpdateRequest(String username, String fName, String lName, String password, Long id) {
     this.username = username;
     this.fName = fName;
     this.lName = lName;
     this.password = password;
+    this.id = id;
 
   }
 }
